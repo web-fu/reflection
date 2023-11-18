b05de60f34a8c99c3a3dfe5514a4df43ceb8b356
web-fu <info@web-fu.it>
2023-11-18 15:29:17 +0100

    Modified phpstan ignore error configurations

--- .config/ignore-by-php-version.neon.php
+++ .config/ignore-by-php-version.neon.php
@@ -2,13 +2,17 @@
 
 $includes = [];
 if (PHP_VERSION_ID < 80100) {
-    $includes[] = __DIR__ . '/readonly-property.neon';
     $includes[] = __DIR__ . '/enum.neon';
     $includes[] = __DIR__ . '/final.neon';
     $includes[] = __DIR__ . '/reflection-function-abstract.neon';
-    $includes[] = __DIR__ . '/reflection-function.neon';
+    $includes[] = __DIR__ . '/reflection-property-readonly.neon';
 }
 
+if (PHP_VERSION_ID < 80200) {
+    $includes[] = __DIR__ . '/reflection-class-readonly.neon';
+    $includes[] = __DIR__ . '/reflection-function.neon';
+    $includes[] = __DIR__ . '/reflection-function-abstract-prototype.neon';
+}
 $config = [];
 $config['includes'] = $includes;
 $config['parameters']['phpVersion'] = PHP_VERSION_ID;
